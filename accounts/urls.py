4d5dd2ff188f2e0470c9922f966592818539600f
archeimisu <archeimisu.acm@gmail.com>
2022-07-02 09:27:31 +0630

    django first commit

---
+++ accounts/urls.py
@@ -0,0 +1,25 @@
+
+from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import path
+
+from . import views
+
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.dashboard,name="dashboard"),
+    path('customers/<int:id>',views.customers,name="customers.show"),
+    path('customer_profile/',views.customer_profile,name='customers.customer_profile'),
+    path('customer_profile/setting',views.customer_profile_setting,name='customers.customer_profile_setting'),
+    path('products/',views.products,name="product"),
+    path('order/create/<int:customerId>',views.orderCreate,name="order.create"),
+    path('order/update/<int:orderId>',views.orderUpdate,name="order.update"),
+    path('order/delete/<int:orderId>',views.orderDelete,name="order.delete"),
+    path('register/',views.register,name='register'),
+    path('login/',views.userLogin,name='login'),
+    path('logout/',views.userLogout,name='logout'),
+    
+]
